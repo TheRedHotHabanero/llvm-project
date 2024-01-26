4bf6efa1ee324700a1eae794b7df8bae483cd2fa
Philip Reames <preames@rivosinc.com>
2024-01-25 18:40:46 -0800

    [RISCV] Minor whitespace as a test change [nfc]

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -20555,7 +20555,6 @@ RISCVTargetLowering::getTargetMMOFlags(const MemSDNode &Node) const {
   MachineMemOperand::Flags TargetFlags = MachineMemOperand::MONone;
   TargetFlags |= (NodeFlags & MONontemporalBit0);
   TargetFlags |= (NodeFlags & MONontemporalBit1);
-
   return TargetFlags;
 }
 
